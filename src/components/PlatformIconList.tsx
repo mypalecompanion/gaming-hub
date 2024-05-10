c416d88fc3e2eedbbc9c39ad99e588b86f91dd77
Jack Kapock <mypalecompanion@gmail.com>
2024-05-10 18:16:23 +0200

    Implemented dynamic list of platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,44 @@
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { BsNintendoSwitch } from "react-icons/bs";
+import {
+  FaAndroid,
+  FaApple,
+  FaFirefoxBrowser,
+  FaLinux,
+  FaPlaystation,
+  FaXbox,
+} from "react-icons/fa";
+import { GrPersonalComputer } from "react-icons/gr";
+import { MdPhoneIphone } from "react-icons/md";
+import { Platform } from "../hooks/useGames";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const iconMap: { [key: string]: IconType } = {
+  pc: GrPersonalComputer,
+  playstation: FaPlaystation,
+  xbox: FaXbox,
+  mac: FaApple,
+  linux: FaLinux,
+  android: FaAndroid,
+  web: FaFirefoxBrowser,
+  nintendo: BsNintendoSwitch,
+  ios: MdPhoneIphone,
+};
+
+const PlatformIconList = ({ platforms }: Props) => {
+  return (
+    <>
+      <HStack color="gray.500" marginY="1">
+        {platforms.map((platform) => (
+          <Icon as={iconMap[platform.slug]} />
+        ))}
+      </HStack>
+    </>
+  );
+};
+
+export default PlatformIconList;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -11,6 +11,13 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
+}
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
 const useGames = () => {
